16b7f0d3dfbde0fcb7b1f47d3b634ce21703ad3f
Thapelo <27338053@student.g.nwu.ac.za>
2019-09-06 01:44:24 +0200

    storeroom dashboard

--- app/src/main/java/com/example/drms/ui/storeroom/StoreroomFragment.java
+++ app/src/main/java/com/example/drms/ui/storeroom/StoreroomFragment.java
@@ -23,13 +23,13 @@ public class StoreroomFragment extends Fragment {
         storeroomViewModel =
                 ViewModelProviders.of(this).get(StoreroomViewModel.class);
         View root = inflater.inflate(R.layout.fragment_storeroom, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
+        /**final TextView textView = root.findViewById(R.id.text_slideshow);
         storeroomViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
         return root;
     }
 }
