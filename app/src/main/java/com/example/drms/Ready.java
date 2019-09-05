6dcdd540e827a4b6d81cabdca97f7664be717196
Nomonde Makgare <27378993@student.g.nwu.ac.za>
2019-09-05 22:38:34 +0200

    Tab configs

---
+++ app/src/main/java/com/example/drms/Ready.java
@@ -0,0 +1,18 @@
+package com.example.drms;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Ready extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
