2836b23a214a8fbf20c2a5fe56e080bc0afadcd6
Nomonde Makgare <27378993@student.g.nwu.ac.za>
2019-09-23 11:21:11 +0200

    Add Sold Activity

---
+++ app/src/main/java/com/example/drms/soldAct.java
@@ -0,0 +1,14 @@
+package com.example.drms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class soldAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sold);
+    }
+}
